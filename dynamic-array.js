e0c6a3db560c2d735d0865405aa0f3efc19ff08e
bohenan <bohenan@umich.edu>
2024-04-01 23:59:15 -0700

    done

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,44 +1,84 @@
 class DynamicArray {
-
-  constructor(defaultSize = 4) {
-
-    // Your code here
+  constructor(size = 4) {
+    this.capacity= size;
+    this.data = new Array(size);
+    this.length = 0;
   }
 
   read(index) {
-
-    // Your code here
+    return (index >= this.length ? undefined : this.data[index]);
   }
 
   push(val) {
-
-    // Your code here
+    if (this.length < this.capacity) {
+      this.data[this.length] = val;
+    } else {
+      this.capacity *= 2;
+      let temp = new Array(this.capacity);
+      for (let i = 0; i < this.length; i++) {
+        temp[i] = this.data[i];
+      }
+      temp[this.length] = val;
+      this.data = temp;
+    }
+    this.length++;
   }
 
 
   pop() {
-
-    // Your code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    let temp = this.data[this.length - 1];
+    this.length--;
+    return temp;
   }
 
   shift() {
-
-    // Your code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    let temp = this.data[0];
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.length--;
+    return temp;
   }
 
   unshift(val) {
-
-    // Your code here
+    if (this.length < this.capacity) {
+      for (let i = this.length; i > 0; i--) {
+        this.data[i] = this.data[i - 1];
+      }
+    } else {
+      this.capacity *= 2;
+      let temp = new Array(this.capacity);
+      for (let i = 0; i < this.length; i++) {
+        temp[i + 1] = this.data[i];
+      }
+      this.data = temp;
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf(val) {
-
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (val === this.data[i]) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   resize() {
-
-    // Your code here
+    this.capacity *= 2;
+    let temp = new Array(this.capacity);
+    for (let i = 0; i < this.length; i++) {
+      temp[i] = this.data[i];
+    }
+    this.data = temp;
   }
 
 }
